34b3a5912237ad07574c428fea62f6f489f4113f
Weslley Lima <weslleyclima@yahoo.com.br>
2021-10-14 21:15:51 -0300

    Exercicio sobre while

---
+++ src/aula141021/MediaNotasDosAlunos.java
@@ -0,0 +1,25 @@
+package aula141021;
+
+import java.util.Scanner;
+
+import aula091021ContaBancaria.EntradaSaidaDados;
+
+/*
+ * Calcular média de X alunos.
+* Notas válidas: entre 0 e 10
+* Contar quantas notas válidas foram digitadas
+* Usuário deve digitar -1 para sair do programa 
+* e ser informado da sua média e quantidade de notas
+ */
+
+public class MediaNotasDosAlunos {
+
+	public static void main(String[] args){
+	
+	int quantidadeNotas = 0;
+	double total = 0.0;
+	double media = 0.0;
+	
+	Scanner entrada = new Scanner(System.in);
+	}
+}
